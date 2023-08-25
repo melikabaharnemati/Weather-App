b9dfc9da267beab4ee8cafcfba617c6006ed17aa
Melika.Baharnemati <melika.baharnemati@gmail.com>
2023-08-25 17:56:34 +0330

    Show Weather

--- main.py
+++ main.py
@@ -5,61 +5,93 @@ from tkinter import ttk, messagebox
 from timezonefinder import TimezoneFinder
 from datetime import datetime
 import requests
-import pytz
+from pytz import timezone
 
 root = Tk()
 root.title("Weather App")
 root.geometry("900x500+300+200")
 root.resizable(False,False)
 
+def getweather():
+    try:
+        city = textfield.get()
+
+        # weather
+        api = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=966b7621b312967e84d45d58f512f596"
+
+        json_data = requests.get(api).json()
+        condition = json_data['weather'][0]['main']
+        description = json_data['weather'][0]['description']
+        temp = int(json_data['main']['temp'] - 273.15)
+        pressure = json_data['main']['pressure']
+        humidity = json_data['main']['humidity']
+        wind = json_data['wind']['speed']
+
+        t.config(text=(temp, "°"))
+        c.config(text=(condition, "|", "Feels", "Like", temp, "°"))
+
+        w.config(text=wind)
+        h.config(text=humidity)
+        d.config(text=description)
+        p.config(text=pressure)
+
+    except Exception as e:
+        messagebox.showerror("Weather App", "Invalid Entry!!")
+
 #search box
 Search_image = PhotoImage(file="images/search.png")
 myimage = Label(image=Search_image)
-myimage.place(x=20,y=20)
+myimage.place(x=200,y=20)
 
 textfield = tk.Entry(root, justify="center", width=17, font=("poppins", 25, "bold"), bg="#404040", border=0, fg="white")
-textfield.place(x=50, y=40)
+textfield.place(x=230, y=40)
 textfield.focus()
 
 Search_icon = PhotoImage(file="images/search_icon.png")
-myimage_icon = Button(image=Search_icon, borderwidth=0, cursor="hand2", bg="#404040")
-myimage_icon.place(x=400, y=34)
+myimage_icon = Button(image=Search_icon, borderwidth=0, cursor="hand2", bg="#404040", command=getweather)
+myimage_icon.place(x=580, y=34)
 
 #logo
 Logo_image = PhotoImage(file="images/weather-logo.png")
 logo = Label(image=Logo_image)
-logo.place(x=150, y=100)
+logo.place(x=150, y=115)
 
 #Bottom-box
 Frame_image = PhotoImage(file="images/box.png")
 frame_myimage = Label(image=Frame_image)
 frame_myimage.pack(padx=5, pady=5, side=BOTTOM)
 
+#time
+name = Label(root, font=("arial", 15, "bold"))
+name.place(x=30, y=100)
+clock = Label(root, font=("Helvetica", 20))
+clock.place(x=30, y=130)
+
 #label
-label1= Label(root, text="WIND", font=("Helvetica", 15, "bold"), fg="white", bg="#1ab5ef")
-label1.place(x=120, y=400)
+label1= Label(root, text="Wind", font=("Helvetica", 15, "bold"), fg="white", bg="#1ab5ef")
+label1.place(x=120, y=410)
 
-label2= Label(root, text="HUMIDITY", font=("Helvetica", 15, "bold"), fg="white", bg="#1ab5ef")
-label2.place(x=240, y=400)
+label2= Label(root, text="Humidity", font=("Helvetica", 15, "bold"), fg="white", bg="#1ab5ef")
+label2.place(x=240, y=410)
 
-label3= Label(root, text="DESCRIPTION", font=("Helvetica", 15, "bold"), fg="white", bg="#1ab5ef")
-label3.place(x=430, y=400)
+label3= Label(root, text="Description", font=("Helvetica", 15, "bold"), fg="white", bg="#1ab5ef")
+label3.place(x=430, y=410)
 
-label4= Label(root, text="PRESSURE", font=("Helvetica", 15, "bold"), fg="white", bg="#1ab5ef")
-label4.place(x=650, y=400)
+label4= Label(root, text="Pressure", font=("Helvetica", 15, "bold"), fg="white", bg="#1ab5ef")
+label4.place(x=650, y=410)
 
 t = Label(font=("arial", 70, "bold"), fg="#ee666d")
 t.place(x=400, y=150)
 c = Label(font=("arial", 15, "bold"))
 c.place(x=400, y=250)
 
-w = Label(text="...", font=("arial", 20, "bold"), bg="#1ab5ef")
-w.place(x=120, y=430)
-h = Label(text="...", font=("arial", 20, "bold"), bg="#1ab5ef")
-h.place(x=280, y=430)
-d = Label(text="...", font=("arial", 20, "bold"), bg="#1ab5ef")
-d.place(x=450, y=430)
-p = Label(text="...", font=("arial", 20, "bold"), bg="#1ab5ef")
-p.place(x=670, y=430)
+w = Label(text="...", font=("arial", 16), bg="#1ab5ef")
+w.place(x=125, y=435)
+h = Label(text="...", font=("arial", 16), bg="#1ab5ef")
+h.place(x=250, y=435)
+d = Label(text="...", font=("arial", 16), bg="#1ab5ef")
+d.place(x=430, y=435)
+p = Label(text="...", font=("arial", 16), bg="#1ab5ef")
+p.place(x=670, y=435)
 
 root.mainloop()
